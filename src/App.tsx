1c134c7b9e2c6792858dd22ddb0627878ad68b10
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-11-15 10:00:58 -0300

    feat(tab): Creates initial data table component This commit introduces the first version of the `DataTable` component for the new tab functionality. - Adds a new React component (`DataTable`) using TypeScript. - Uses the Material-UI `DataGrid` to display data in table format. - Defines static columns and sample data for development and demonstration. - Includes initial pagination configuration and basic styling with the Material-UI `Paper` component.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DataTable from "./components/tab.tsx";
+
+export default function App() {
+  return (
+    <>
+      <div className="m-6">
+        <DataTable />
+      </div>
+    </>
+  );
+}
